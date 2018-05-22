a026db522f5d4e6238c741e2412125ed2c34ea71
Allen Hsu <bboypupu@gmail.com>
2018-05-22 06:55:35 -0700

    test demo app for object recognition

---
+++ test_core_ml_and_metal/test_core_ml_and_metalUITests/test_core_ml_and_metalUITests.swift
@@ -0,0 +1,36 @@
+//
+//  test_core_ml_and_metalUITests.swift
+//  test_core_ml_and_metalUITests
+//
+//  Created by Yuan-Pu Hsu on 5/6/18.
+//  Copyright © 2018 Yuan-Pu Hsu. All rights reserved.
+//
+
+import XCTest
+
+class test_core_ml_and_metalUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
