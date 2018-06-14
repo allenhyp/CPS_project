c5e6a346d5c290469defc97d99f7c3cb37b24fff
Allen Hsu <bboypupu@gmail.com>
2018-06-14 16:07:50 -0700

    average runtime

--- server_ml_script/tsc_cnn.py
+++ server_ml_script/tsc_cnn.py
@@ -28,28 +28,34 @@ def preprocess_img(img):
     return img
 
 def main():
-	main_start_time = time.time()
-	root_dir = '/home/allenhsu/uploads/'
-	X_test = []
-	all_img_paths = glob.glob(os.path.join(root_dir, '*.png'))
-	for img_path in all_img_paths:
-		try:
-			img = preprocess_img(io.imread(img_path))
-			X_test.append(img)
-		except(IOError, OSError):
-			print('missed', img_path)
-			pass
-	X_test = np.array(X_test)
+	file = open('runtime.txt','a')
 	model = load_model("traffic_sign_classifier_model.h5")
-	classification_start_time = time.time()
-	y_pred = model.predict_classes(X_test)
-	# print("classification time: {}".format(time.time() - classification_start_time))
-	# print("python script running time: {}".format(time.time() - main_start_time))
-	result = '{0}_{1}_{2}\n'.format(y_pred, time.time() - classification_start_time, time.time() - main_start_time)
-	print(result)
+	for j in range(5):
+		main_start_time = time.time()
+		root_dir = '/home/allenhsu/uploads/'
+		# root_dir = 'testing40/'
+		X_test = []
+		all_img_paths = glob.glob(os.path.join(root_dir, '*.png'))
+		for img_path in all_img_paths:
+			try:
+				img = preprocess_img(io.imread(img_path))
+				X_test.append(img)
+			except(IOError, OSError):
+				print('missed', img_path)
+				pass
+		X_test = np.array(X_test)
+		file.write('preprocess_time: {0}\n'.format(time.time() - main_start_time))
+		for i in range(10):
+			classification_start_time = time.time()
+			y_pred = model.predict_classes(X_test)
+			# print("classification time: {}".format(time.time() - classification_start_time))
+			# print("python script running time: {}".format(time.time() - main_start_time))
+			# result = '{0}_{1}_{2}\n'.format(y_pred, time.time() - classification_start_time, time.time() - main_start_time)
+			# print(result)
+			model_runtime = time.time() - classification_start_time
+			runtime = '{0}, {1} per image\n'.format(model_runtime, model_runtime/len(y_pred))
 
-	file = open('result.txt','a')
-	file.write(result)
+			file.write(runtime)
 	file.close()
 
 if __name__ == "__main__":
